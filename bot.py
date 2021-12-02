ebac67263b4e15aa14484265cec2e99006f84d28
vandalise <95403169+vandalise@users.noreply.github.com>
2021-12-02 01:16:10 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-#from dhooks import Webhook
+from dhooks import Webhook
 
-#hook = Webhook("input webhook here")
-
-urls = []
-while True:
+for i in range(3):
     keywords = ["put", "your", "keywords", "like", "this"] 
     for keyword in keywords:
-        r = requests.get(f'https://disboard.org/servers/tag/{keyword}/1', ).text
-        if "Too Many Requests" in r:
-            print("Ratelimited")
-            #this doesnt really matter, i just added it so theres something to look at
-        else:
-            print("Working")
+        r = requests.get(f'https://disboard.org/servers/tag/{keyword}/{i}', ).text
         servers = BeautifulSoup(r, 'html.parser').findAll('div', class_="column is-one-third-desktop is-half-tablet")
         for server in servers:
             eyedee = server.find('a', class_="button button-join is-discord").get('data-id')
-            if eyedee in urls:
-                pass
-            else:
-                urls.append(eyedee)
-                url = f"https://disboard.org/site/get-invite/{eyedee}"
-                x = requests.get(url)
-                tempinvite = x.text
-                invite = tempinvite.replace('"', '')
-                print(invite)
-                #remove #'s if you want to send the invite to a webhook
-                #hook.send(f"{invite}  | ``https://disboard.org/server/{eyedee}``")
-    time.sleep(5)
+            url = f"https://disboard.org/site/get-invite/{eyedee}"
+            x = requests.get(url)
+            tempinvite = x.text
+            invite = tempinvite.replace('"', '')
+            print(invite)
